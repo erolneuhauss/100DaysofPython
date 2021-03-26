811518c94c5f51723fee161c8e07098293a91bdc
eneuhauss <erol.neuhauss@gmx.de>
2021-03-26 21:11:57 +0100

    inital commit

---
+++ day-4-rock-paper-scissors-start.py
@@ -0,0 +1,53 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+game_images = [ rock, paper, scissors ]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >=3 or user_choice < 0:
+  print("You typed an invalid number, you loose")
+else:
+  print(game_images[user_choice])
+  
+  computer_choice = random.randint(0,2)
+  print(f"Computer chose:")
+  print(game_images[computer_choice])
+  
+  if user_choice == 0 and computer_choice == 1:
+    print("You loose!")
+  elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif user_choice == 1 and computer_choice == 0:
+    print("You win!")
+  elif user_choice == 1 and computer_choice == 2:
+    print("You loose!")
+  elif user_choice == 2 and computer_choice == 0:
+    print("You loose!")
+  elif user_choice == 2 and computer_choice == 1:
+    print("You win!")
+  else:
+    print("It's a tie!")
