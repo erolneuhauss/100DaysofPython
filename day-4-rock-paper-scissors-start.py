e5e105869908bfc922847ff796f63265888ef0cf
eneuhauss <erol.neuhauss@gmx.de>
2021-03-26 23:54:34 +0100

    clean up using pylama

--- day-4-rock-paper-scissors-start.py
+++ day-4-rock-paper-scissors-start.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,29 +25,29 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-import random
-game_images = [ rock, paper, scissors ]
+game_images = [rock, paper, scissors]
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_choice >=3 or user_choice < 0:
-  print("You typed an invalid number, you loose")
+user_choice = int(input("What do you choose? Type 0 for Rock,",
+                        "1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you loose")
 else:
-  print(game_images[user_choice])
-  computer_choice = random.randint(0,2)
-  print(f"Computer chose:")
-  print(game_images[computer_choice])
-  
-  if user_choice == 0 and computer_choice == 1:
-    print("You loose!")
-  elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-  elif user_choice == 1 and computer_choice == 0:
-    print("You win!")
-  elif user_choice == 1 and computer_choice == 2:
-    print("You loose!")
-  elif user_choice == 2 and computer_choice == 0:
-    print("You loose!")
-  elif user_choice == 2 and computer_choice == 1:
-    print("You win!")
-  else:
-    print("It's a tie!")
+    print(game_images[user_choice])
+    computer_choice = random.randint(0, 2)
+    print(f"Computer chose:")
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 1:
+        print("You loose!")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win!")
+    elif user_choice == 1 and computer_choice == 2:
+        print("You loose!")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You loose!")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You win!")
+    else:
+        print("It's a tie!")
