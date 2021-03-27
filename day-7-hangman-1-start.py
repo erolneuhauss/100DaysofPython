ad0afe647946879d98a99b663bdcfc03d169a990
eneuhauss <erol.neuhauss@gmx.de>
2021-03-28 00:41:33 +0100

    using an f-string that does not have any interpolated variables

--- day-7-hangman-1-start.py
+++ day-7-hangman-1-start.py
@@ -11,19 +11,19 @@ print(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter
 # and assign their answer to a variable called guess. Make guess lowercase.
-guess = input(f"Guess a letter: ").lower()
-print(f"Your have guessed:", guess)
+guess = input("Guess a letter: ").lower()
+print("Your have guessed:", guess)
 
 # TODO-3 - Check if the letter the user guessed (guess)
 # is one of the letters in the chosen_word
 if guess in chosen_word:
-    print(guess, f" is in the word")
+    print(guess, " is in the word")
 else:
-    print(f"You have guessed wrong")
+    print("You have guessed wrong")
 
 # Finding all indexes of a string in the list
 # with while loop
-print(f"----- while loop -----")
+print("----- while loop -----")
 matched_indexes = []
 index = 0
 length = len(chosen_word)
@@ -35,7 +35,7 @@ while index < length:
         print("Wrong")
     index += 1
 
-print(f"----- for loop if equal --")
+print("----- for loop if equal --")
 # with for loop
 for letter in chosen_word:
     if letter == guess:
@@ -43,7 +43,7 @@ for letter in chosen_word:
     else:
         print("Wrong")
 
-print(f"----- for loop if in -----")
+print("----- for loop if in -----")
 # with for loop
 for letter in chosen_word:
     if letter in guess:
