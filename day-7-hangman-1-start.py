80f81e28c0169772d821ab92f3db6d6e0970fa84
eneuhauss <erol.neuhauss@gmx.de>
2021-03-27 12:34:48 +0100

    works until guess included

--- day-7-hangman-1-start.py
+++ day-7-hangman-1-start.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Step 1
 import random
 
@@ -5,9 +6,13 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and
 # assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter
 # and assign their answer to a variable called guess. Make guess lowercase.
+guess = input(f"Guess a letter: ")
+guess = guess.lower()
 
 # TODO-3 - Check if the letter the user guessed (guess)
 # is one of the letters in the chosen_word
