9b28f6a3025bf89f76af432c4afbd68bc30bda3c
eneuhauss <erol.neuhauss@gmx.de>
2021-03-28 22:56:15 +0200

    alternative with while not and if not

--- day-7-hangman-3-start.py
+++ day-7-hangman-3-start.py
@@ -26,3 +26,18 @@ while '_' in display:
             display[position] = letter
     print(display)
 print(display, "You have won!")
+
+# alternative solution
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+#Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+#        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+print(display, "You have won!")
