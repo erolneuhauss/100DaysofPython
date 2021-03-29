0a1bd98c0279049bcdb36e7499e24f53c45d86c2
eneuhauss <erol.neuhauss@gmx.de>
2021-03-30 00:04:33 +0200

    make it more readable

--- day-7-hangman-5-start.py
+++ day-7-hangman-5-start.py
@@ -2,6 +2,7 @@
 # Step 5
 
 import random
+import os
 # TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 # TODO-2: - Import the stages from hangman_art.py and make this error go away.
 # TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
@@ -26,11 +27,12 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    os.system('cls||clear')
 
     # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
 
     if guess in display:
-        print(f"You have guessed {guess}, already!")
+        print(f"You have guessed \"{guess}\", already!")
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -41,11 +43,11 @@ while not end_of_game:
     # Check if user is wrong.
     if guess not in chosen_word:
         # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-        print(f"{guess}, is not in the word, sorry. You lose a life!")
+        print(f"\"{guess}\", is not in the word, sorry. You lose a life!")
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose.")
+            print(f"You lose! The word was: \"{chosen_word}\"")
 
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
