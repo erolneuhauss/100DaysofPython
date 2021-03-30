14cbfeaa8637d27285ce33c33d118bf81bcfd151
eneuhauss <erol.neuhauss@gmx.de>
2021-03-30 19:42:01 +0200

    input statement in a print statement

---
+++ day-1-printing-start.py
@@ -0,0 +1,15 @@
+# Write your code below this line 👇
+print("Hello World!\n")
+
+print("Hello " + input("What is your name?") + "!\n")
+
+print("-----------------")
+print("private excursion")
+print("-----------------\n")
+
+a = 'Erol'
+b = 'greets'
+c = 'World'
+print(a + " " + b,"the",c,"\n")
+a = input("What is your name?\n")
+print("You have typed: " + a)
