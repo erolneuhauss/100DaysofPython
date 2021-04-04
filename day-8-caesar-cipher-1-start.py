e4298b17045012333cc38a9529b380287760470a
eneuhauss <erol.neuhauss@gmx.de>
2021-04-04 20:34:27 +0200

    progress

---
+++ day-8-caesar-cipher-1-start.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#text = input("Type your message:\n").lower()
+#shift = int(input("Type the shift number:\n"))
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+direction = "encode"
+text = "civilization"
+shift = 1
+def encrypt(text, shift):
+    if direction == "encode":
+        cipher_text = ''
+        for letter in text:
+            position = alphabet.index(letter)
+            new_pos = position + shift
+            if new_pos > len(alphabet) - 1:
+                new_pos -= len(alphabet)
+                cipher_text = cipher_text + alphabet[new_pos]
+            else:
+                cipher_text = cipher_text + alphabet[new_pos]
+        print(f"The encoded text is {cipher_text}")
+    else:
+        cipher_text = ''
+        for letter in text:
+            position = alphabet.index(letter)
+            new_pos = int(position) - shift
+            cipher_text = cipher_text + alphabet[new_pos]
+        print(f"The encoded text is {cipher_text}")
+encrypt(text, shift)
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text'
+    # forwards in the alphabet by the shift amount and print the encrypted text.
+    #e.g.
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+# TODO-3: Call the encrypt function and pass in the user inputs.
+# You should be able to test the code and encrypt a message.
---
+++ day-8-start.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("Hello, Erol!")
+    print("How are you, doing?")
+    print("The weather is nice!")
+greet()
+print("--------------------------")
+
+def greet_a(name):
+    print(f"Hello, {name}!")
+    print(f"How are you doing, {name}?")
+    print("The weather is nice!")
+greet_a("Erol")
+greet_a("Angela")
+print("--------------------------")
+
+def greet_with(sender, receiver):
+    print(f"Hello, {receiver}!")
+    print(f"I am {sender}")
+    print("How are you doing?")
+greet_with("Erol", "Angela")
+print("--------------------------")
+
+def greet_with(sender, receiver):
+    print(f"Hello, {receiver}!")
+    print(f"I am {sender}")
+    print("How are you doing?")
+greet_with(sender="Erol", receiver="Angela")
+print("--------------------------")
