4c05c027345a0f7fe4f4cacb70b56484ed31ae10
eneuhauss <erol.neuhauss@gmx.de>
2021-04-20 13:29:54 +0200

    updated by pycharm

--- day-14-higher-lower-solution.py
+++ day-14-higher-lower-solution.py
@@ -5,13 +5,16 @@ import random
 from diverse_logos import higher_lower_logo, higher_lower_vs
 from game_data import data
 
+
 def format_data(account):
     """Format the account data and return into printable format."""
     account_name = account["name"]
     account_desc = account["description"]
     account_country = account["country"]
     account_follower_count = account["follower_count"]
-    return f"{account_name}, a {account_desc}, from {account_country}"
+    return f"{account_name}, a {account_desc}, from {account_country}" \
+           f"(Pss! Hint: Follower count: {account_follower_count})"
+
 
 def check_answer(guess, a_followers, b_followers):
     """Take the user guess and followers count and returns if they got it right"""
@@ -19,6 +22,7 @@ def check_answer(guess, a_followers, b_followers):
         return guess == "a"
     else:
         return guess == "b"
+
 
 # Display art
 print(higher_lower_logo)
@@ -28,7 +32,8 @@ account_b = random.choice(data)
 
 # Make the game repeatable.
 while game_should_continue:
-# Generate a random account from the game data.
+    # Generate a random account from the game data.
+    # Make account at position B become the next account at position A.
     account_a = account_b
     account_b = random.choice(data)
 
@@ -38,29 +43,24 @@ while game_should_continue:
     print(higher_lower_vs)
     print(f"Against B: {format_data(account_b)}.")
 
-# Ask user for a guess.
+    # Ask user for a guess.
     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# Check if user is correct.
-## Get follower_count count of each account.
+    # Check if user is correct.
+    ## Get follower_count count of each account.
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
-## Use if statement to check if user is correct.
+    ## Use if statement to check if user is correct.
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
-# Clear the screen
+    # Clear the screen
     os.system('cls||clear')
     print(higher_lower_logo)
 
-# Give user feedback on their guess.
-# Score keeping.
+    # Give user feedback on their guess.
+    # Score keeping.
     if is_correct:
         score += 1
         print(f"You're right! Current score: {score}")
     else:
         game_should_continue = False
         print(f"Sorry, that's wrong. Your final score: {score}")
-
-
-
-# Make account at position B become the next account at position A.
-
