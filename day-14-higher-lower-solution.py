ec47d96888e7c301d2fd9b5193c6c3da49568559
eneuhauss <erol.neuhauss@gmx.de>
2021-04-20 11:51:51 +0200

    go throught the solution with Angela

---
+++ day-14-higher-lower-solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+from random import choice
+from diverse_logos import higher_lower_logo, higher_lower_vs
+from game_data import data
+
+# Display art
+
+# Generate a random account from the game data.
+
+# Format the account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower_count count of each account.
+## Use if statement to check if user is correct.
+
+# Give user feedback on their guess.
+
+# Score keeping.
+
+# Make the game repeatable.
+
+# Make account at position B become the next account at position A.
+
