2016d7c8c3ca3d7f0429b8beab868d25c2af6f89
eneuhauss <erol.neuhauss@gmx.de>
2021-04-20 18:24:18 +0200

    have black reformat for me

--- day-14-higher-lower-start.py
+++ day-14-higher-lower-start.py
@@ -2,33 +2,42 @@
 
 import os
 from random import choice
+
 from diverse_logos import higher_lower_logo, higher_lower_vs
 from game_data import data
+
 
 def determine_answer(A, B):
     """
     determine right answer by comparing follower_count between A and B
     """
-    if A['follower_count'] > B['follower_count']:
-        answer = 'A'
+    if A["follower_count"] > B["follower_count"]:
+        answer = "A"
         return answer
     else:
-        answer = 'B'
+        answer = "B"
         return answer
+
 
 print(higher_lower_logo)
 score = 0
 is_game_over = False
 A = choice(data)
 while not is_game_over:
-    print(f"\nCompare A: {A['name']}, {A['description']}, {A['country']}, {A['follower_count']}.")
+    print(
+        f"\nCompare A: {A['name']}, {A['description']}, {A['country']}, "
+        f"{A['follower_count']}."
+    )
     print(higher_lower_vs)
     B = choice(data)
-    print(f"Against B: {B['name']}, {B['description']}, {B['country']}, {B['follower_count']}.")
+    print(
+        f"Against B: {B['name']}, {B['description']}, {B['country']}, "
+        f"{B['follower_count']}."
+    )
     guess = input("Who has more followers? Type 'A' or 'B': ")
     answer = determine_answer(A, B)
 
-    os.system('cls||clear')
+    os.system("cls||clear")
     print(higher_lower_logo)
     if guess == answer:
         score += 1
@@ -37,7 +46,3 @@ while not is_game_over:
     else:
         print(f"Sorry, that's wrong. Final score: {score} ")
         is_game_over = True
-
-
-
-
