ea9a1223212d35da12f2ac3521d48cfec74e5d25
eneuhauss <erol.neuhauss@gmx.de>
2021-04-21 23:48:04 +0200

    progess on python coffee machine. Main TODO: implement repeatability

--- day_15_coffe_machine_start.py
+++ day_15_coffe_machine_start.py
@@ -7,11 +7,15 @@ from day_15_coffe_machine_data import MENU, resources
 
 # TODO: Prompt user by asking "What would you like? (espresso/latte/cappucino)"
 userInput = input("What would you like? (espresso/latte/cappucino) ")
-print(userInput)
+
+if userInput.startswith("e"):
+    order = "espresso"
+print(f"You have ordered {order}")
 
 # TODO: Print report by entering "report",
 #       which reveals coffee machines resources
-resources["money"] = 0
+resources.setdefault("money", 0)
+MENU["espresso"]["ingredients"].setdefault("milk", 0)
 water = resources["water"]
 milk = resources["milk"]
 coffee = resources["coffee"]
@@ -25,7 +29,6 @@ if userInput.startswith("r"):
         f"Money: ${money}"
     )
 
-
 # TODO: Exit emulator by entering "off"
 if userInput.startswith("o"):
     exit()
@@ -37,25 +40,64 @@ if userInput.startswith("e"):
         MENU["espresso"]["ingredients"]["water"] < water
         and MENU["espresso"]["ingredients"]["coffee"] < coffee
     ):
-        print("We have enoug resources")
+        print("We have enough resources")
         print(f"That will be ${MENU['espresso']['cost']}")
     else:
         print("Sorry there is not enough resources")
 
 # TODO: Process coins: amounts of quaters, dimes, nicles and pennies
-insertQuaters = int(input("Quaters"))
-insertDimes = int(input("Dimes"))
-insertNickles = int(input("Nickles"))
-insertPennies = int(input("Pennies"))
+quaters = 0.25
+dimes = 0.1
+nickles = 0.05
+pennies = 0.01
+
+print("Please insert coins.")
+insertedQuaters = int(input("how many quaters? "))
+insertedDimes = int(input("how many dimes? "))
+insertedNickles = int(input("how many nickles? "))
+insertedPennies = int(input("how many pennies? "))
 # TODO: Calculate monetary value of inserted coins
 
-# TODO: Check transaction successful
+totalAmountInserted = round(
+    insertedQuaters * quaters
+    + insertedDimes * dimes
+    + insertedNickles * nickles
+    + insertedPennies * pennies,
+    2,
+)
 
-# TODO: Make coffee
+print(f"You have inserted ${totalAmountInserted}")
+
+# TODO: Check transaction successful
+# TODO: offer change, in case the user has inserted too much money
+if totalAmountInserted > MENU["espresso"]["cost"]:
+    change = totalAmountInserted - MENU["espresso"]["cost"]
+    print(
+        "Thank you for your order."
+        f" Here is ${change} in change."
+        " Preparing your coffee now."
+    )
+elif totalAmountInserted == MENU["espresso"]["cost"]:
+    print("Thank you. Preparing your coffee now.")
+else:
+    print("Sorry that is not enough money. Money refunded")
 
 # TODO: Deduce ingredients to make the coffee
 #       from the coffee machines resources
+water = resources["water"] - MENU["espresso"]["ingredients"]["water"]
+milk = resources["milk"] - MENU["espresso"]["ingredients"]["milk"]
+coffee = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+money = resources["money"] + MENU["espresso"]["cost"]
+
+print(
+    "Remaining resources in the machine:\n"
+    f"Water: {water}ml\n"
+    f"Milk: {milk}ml\n"
+    f"Coffee: {coffee}g\n"
+    f"Money: ${money}"
+)
 
 # TODO: Print out statement "Here is your [coffee product]. Enjoy!"
+print(f"Here is your {order} ☕. Enjoy")
 
 # TODO: LOOP to "What would you like? (espresso/latte/cappucino)"
