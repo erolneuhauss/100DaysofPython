298ac92f64ebc91f6fc2471d5960b787166a2158
eneuhauss <erol.neuhauss@gmx.de>
2021-04-23 12:27:18 +0200

    moved and rename for working with pycharm

---
+++ day_15_coffee_machine/main_my_solution.py
@@ -0,0 +1,141 @@
+"""Virtual Coffee Machine"""
+from data import MENU, coin_types, resources
+
+# in the begining there is no resources["money"], therefore set a default
+resources.setdefault("money", 0)
+
+
+# Check which action to take
+def take_action(action):
+    """Evalute what action to take: turn off, print report or prepare drink"""
+    if action == "off":
+        return 1
+    if action.startswith("r"):  # report
+        return 2
+    if action.startswith("e"):
+        drink = "espresso"
+        return drink
+    if action.startswith("l"):
+        drink = "latte"
+        return drink
+    if action.startswith("c"):
+        drink = "cappuccino"
+        return drink
+
+
+# Print report by entering "report",
+def generate_report():
+    """Generate report"""
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+    money = resources["money"]
+    money_with_2_decimals = "{:.2f}".format(money)
+
+    print(
+        f"Water: {water}ml\n"
+        f"Milk: {milk}ml\n"
+        f"Coffee: {coffee}g\n"
+        f"Money: ${money_with_2_decimals}"
+    )
+
+
+# Check resources, when user chooses a drink
+def check_ordered_drink_against_resources(drink):
+    """Check resources, when user chooses a drink"""
+    selected_ingredients = MENU.get(drink)["ingredients"]
+    for ingredient in selected_ingredients:
+        amount = selected_ingredients[ingredient]
+        if amount > resources[ingredient]:
+            print(
+                f"{amount} units of {ingredient} needed but only "
+                f"{resources[ingredient]} units available 😭.\n"
+                f"Sorry, there is not enough {ingredient}. "
+                "Call service for refill."
+            )
+            return 0
+        return 1
+
+
+# Process coins: amounts of quaters, dimes, nicles and pennies
+def process_coins(drink):
+    """Handles inserted coins"""
+    cost_of_drink = MENU[drink]["cost"]
+    cost_with_2_decimals = "{:.2f}".format(cost_of_drink)
+
+    total_amount_inserted = 0
+    print(f"Please insert coins worth of ${cost_with_2_decimals}:")
+    # Calculate monetary value of inserted coins
+    for coin_type, value in coin_types.items():
+        try:
+            inserted_coins = int(input(f"how many {coin_type} (${value})? "))
+            total_amount_inserted += inserted_coins * value
+            total_with_2_decimals = "{:.2f}".format(total_amount_inserted)
+            amount_left_to_pay = cost_of_drink - total_amount_inserted
+            amount_left_with_2_decimals = "{:.2f}".format(amount_left_to_pay)
+            print(f"Sum: ${total_with_2_decimals}")
+            # Check transaction successful
+            if total_amount_inserted >= cost_of_drink:
+                # You've made profit. Add cost of drink to resources["money"]
+                resources["money"] += cost_of_drink
+                break
+            print(f"Remaining amount to pay: ${amount_left_with_2_decimals}")
+        except (TypeError, ValueError):
+            continue
+
+    # offer change, in case the user has inserted too much money
+    if total_amount_inserted == 0:
+        print("You did not insert any coins. No coffee for you then.")
+    elif total_amount_inserted == cost_of_drink:
+        print("Thank you. Preparing your coffee now.")
+        return 0
+    elif amount_left_to_pay < 0:
+        change = amount_left_to_pay * -1
+        change_with_2_decimals = "{:.2f}".format(change)
+        print(
+            "Thank you for your order."
+            f" Here is ${change_with_2_decimals} in change."
+            " Preparing your coffee now."
+        )
+        return 1
+    else:
+        print("Sorry that is not enough money. Money refunded")
+        return 2
+
+
+# Deduce ingredients to make the coffee
+def deduce_ingredients(drink):
+    """Deduces ingredients to make the coffee"""
+    print("Deduce ingredients")
+    selected_ingredients = MENU.get(drink)["ingredients"]
+    for ingredient in selected_ingredients:
+        consumed_ingredient = selected_ingredients[ingredient]
+        print(ingredient, consumed_ingredient)
+        resources[ingredient] = resources[ingredient] - consumed_ingredient
+
+
+# Start of program
+MACHINE_ON = True
+# Prompt shows every time when an action is completed
+while MACHINE_ON:
+    # Prompt user: "What would you like? (espresso/latte/cappucino)"
+    user_input = input("What would you like? (espresso/latte/cappuccino) ")
+    non_drink_action = take_action(user_input)
+    if non_drink_action is None:
+        continue  # empty string restarts if-control from the top
+    if non_drink_action == 1:
+        MACHINE_ON = False
+        print("Turning off machine for maintenance")
+    elif non_drink_action == 2:
+        generate_report()
+    else:
+        DRINK = take_action(user_input)
+        RESOURCES_READY = check_ordered_drink_against_resources(DRINK)
+        if RESOURCES_READY == 1:
+            SUFFICIENT_COINS = process_coins(DRINK)
+            if SUFFICIENT_COINS is None:
+                continue  # empty string restarts if-control from the top
+            if SUFFICIENT_COINS <= 1:
+                deduce_ingredients(DRINK)
+                # End of program
+                print(f"Here is your {DRINK} ☕. Enjoy")
---
+++ day_15_coffee_machine/pure_statements.py
@@ -0,0 +1,111 @@
+"""
+This program emulates a coffee machine like the one here:
+https://replit.com/@appbrewery/coffee-machine-final?embed=1&output=1#main.py
+
+"""
+from data import MENU, resources
+
+
+# TODO: Check which action to take
+def takeAction():
+    if userInput.startswith("e"):
+        order = "espresso"
+        print(f"You have ordered {order}")
+        return order
+
+
+# TODO: Prompt user by asking "What would you like? (espresso/latte/cappucino)"
+# TODO: Prompt should show every time when an action is completed (LOOP)
+userInput = input("What would you like? (espresso/latte/cappuccino) ")
+
+order = takeAction()
+
+# TODO: Print report by entering "report",
+#       which reveals coffee machines resources
+resources.setdefault("money", 0)
+MENU["espresso"]["ingredients"].setdefault("milk", 0)
+water = resources["water"]
+milk = resources["milk"]
+coffee = resources["coffee"]
+money = resources["money"]
+
+if userInput.startswith("r"):
+    print(
+        f"Water: {water}ml\n"
+        f"Milk: {milk}ml\n"
+        f"Coffee: {coffee}g\n"
+        f"Money: ${money}"
+    )
+
+# TODO: Exit emulator by entering "off"
+if userInput.startswith("o"):
+    exit()
+
+# TODO: Check resources, when user chooses a drink
+if userInput.startswith("e"):
+    print(MENU["espresso"])
+    if (
+        MENU["espresso"]["ingredients"]["water"] < water
+        and MENU["espresso"]["ingredients"]["coffee"] < coffee
+    ):
+        print("We have enough resources")
+        print(f"That will be ${MENU['espresso']['cost']}")
+    else:
+        print("Sorry there is not enough resources")
+
+# TODO: Process coins: amounts of quaters, dimes, nicles and pennies
+quaters = 0.25
+dimes = 0.1
+nickles = 0.05
+pennies = 0.01
+
+print("Please insert coins.")
+insertedQuaters = int(input("how many quaters? "))
+insertedDimes = int(input("how many dimes? "))
+insertedNickles = int(input("how many nickles? "))
+insertedPennies = int(input("how many pennies? "))
+
+# TODO: Calculate monetary value of inserted coins
+totalAmountInserted = round(
+    insertedQuaters * quaters
+    + insertedDimes * dimes
+    + insertedNickles * nickles
+    + insertedPennies * pennies,
+    2,
+)
+
+print(f"You have inserted ${totalAmountInserted}")
+
+# TODO: Check transaction successful
+# TODO: offer change, in case the user has inserted too much money
+if totalAmountInserted > MENU["espresso"]["cost"]:
+    change = totalAmountInserted - MENU["espresso"]["cost"]
+    print(
+        "Thank you for your order."
+        f" Here is ${change} in change."
+        " Preparing your coffee now."
+    )
+elif totalAmountInserted == MENU["espresso"]["cost"]:
+    print("Thank you. Preparing your coffee now.")
+else:
+    print("Sorry that is not enough money. Money refunded")
+
+# TODO: Deduce ingredients to make the coffee
+#       from the coffee machines resources
+water = resources["water"] - MENU["espresso"]["ingredients"]["water"]
+milk = resources["milk"] - MENU["espresso"]["ingredients"]["milk"]
+coffee = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+money = resources["money"] + MENU["espresso"]["cost"]
+
+print(
+    "Remaining resources in the machine:\n"
+    f"Water: {water}ml\n"
+    f"Milk: {milk}ml\n"
+    f"Coffee: {coffee}g\n"
+    f"Money: ${money}"
+)
+
+# TODO: Print out statement "Here is your [coffee product]. Enjoy!"
+print(f"Here is your {order} ☕. Enjoy")
+
+# TODO: LOOP to "What would you like? (espresso/latte/cappucino)"
